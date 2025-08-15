8acc10e38879d67e21a525d8cec6256ef5e9e5fc
yangjoonhyung <kangyang0617@gmail.com>
2025-08-16 02:29:12 +0900

    first update

--- src/main/java/nextdoor/project/user/repository/UserRepository.java
+++ src/main/java/nextdoor/project/user/repository/UserRepository.java
@@ -1,4 +1,20 @@
 package nextdoor.project.user.repository;
 
+import nextdoor.project.user.User;
+
+import java.util.List;
+
 public interface UserRepository {
+
+    User save(User user);
+
+    User findById(String userId);
+
+    List<User> findAll();
+
+    void changePassword(String userId, String changePw);
+
+    void delete(String userId);
+
+
 }
--- src/main/java/nextdoor/project/user/repository/jpa/JpaUserRepository.java
+++ src/main/java/nextdoor/project/user/repository/jpa/JpaUserRepository.java
@@ -1,6 +1,34 @@
 package nextdoor.project.user.repository.jpa;
 
+import nextdoor.project.user.User;
 import nextdoor.project.user.repository.UserRepository;
 
+import java.util.List;
+
 public class JpaUserRepository implements UserRepository {
+
+    @Override
+    public User save(User user) {
+        return null;
+    }
+
+    @Override
+    public User findById(String userId) {
+        return null;
+    }
+
+    @Override
+    public List<User> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public void changePassword(String userId, String changePw) {
+
+    }
+
+    @Override
+    public void delete(String userId) {
+
+    }
 }
