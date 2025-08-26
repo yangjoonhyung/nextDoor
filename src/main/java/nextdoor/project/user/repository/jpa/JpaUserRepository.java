fc253da74e32887cf63c1908331140a73d014746
yangjoonhyung <kangyang0617@gmail.com>
2025-08-26 23:56:58 +0900

    jpa 제작 완료

--- src/main/java/nextdoor/project/user/repository/jpa/JpaUserRepository.java
+++ src/main/java/nextdoor/project/user/repository/jpa/JpaUserRepository.java
@@ -1,39 +1,58 @@
 package nextdoor.project.user.repository.jpa;
 
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
 import nextdoor.project.user.User;
 import nextdoor.project.user.repository.UserRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
+@RequiredArgsConstructor
 public class JpaUserRepository implements UserRepository {
+
+    private final EntityManager em;
 
     @Override
     public User save(User user) {
-        return null;
+        em.persist(user);
+        return user;
     }
 
     @Override
     public User findById(String userId) {
-        return null;
+        User user = em.find(User.class, userId);
+        return user;
     }
 
     @Override
     public List<User> findAll() {
-        return List.of();
+        String jpql = "select u from User u";
+        List resultList = em.createQuery(jpql).getResultList();
+        return resultList;
     }
 
     @Override
     public void changePassword(String userId, String changePw) {
-
+        User user = em.find(User.class, userId);
+        user.setPassword(changePw);
     }
 
     @Override
     public void delete(String userId) {
-
+        User user = em.find(User.class, userId);
+        em.remove(user);
     }
 
     @Override
     public User findByEmailAndName(String email, String name) {
-        return null;
+        String jpql = "select u form User u where u.email = :email and u.name = :name";
+        User result = em.createQuery(jpql, User.class)
+                .setParameter("email", email)
+                .setParameter("name", name)
+                .getSingleResult();
+
+        return result;
     }
 }
