f0d9fc2659bdbd1c3407a5a868db463f76067edb
yangjoonhyung <kangyang0617@gmail.com>
2025-09-15 01:21:45 +0900

    공공데이터 제저ㅏㄱ 완료

---
+++ src/main/java/nextdoor/project/cart/repository/jpa/JpaCartRepository.java
@@ -0,0 +1,38 @@
+package nextdoor.project.cart.repository.jpa;
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import nextdoor.project.cart.Cart;
+import nextdoor.project.cart.repository.CartRepository;
+import nextdoor.project.user.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class JpaCartRepository implements CartRepository {
+
+    private final EntityManager em;
+
+    @Override
+    public Cart save(Cart cart) {
+        em.persist(cart);
+        return cart;
+    }
+
+    @Override
+    public List<Cart> findByUser(User user) {
+        String jpql = "select cart from cart c where c.user.userId = :userId";
+        List<Cart> resultList = em.createQuery(jpql, Cart.class)
+                .setParameter("userId", user.getUserId())
+                .getResultList();
+
+        return resultList;
+    }
+
+    @Override
+    public void deleteByUserAndPlaceId(User user, String placeId) {
+        
+    }
+}
